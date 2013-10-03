b87c3fa206460ad64aff95ca20aa3604e474dee4
victor <ilovemayalopez@gmail.com>
2013-10-03 17:32:02 +0200

    News attributes and methods

--- lib/wortru.rb
+++ lib/wortru.rb
@@ -2,28 +2,33 @@ require 'roxml'
 require 'pry'
 
 class Wortru
-	
-	class Category
-		include ROXML
-		xml_reader :domain, from: :attr
-		xml_reader :nicename, from: :attr
-		xml_reader :name, from: :content
+  
+  class Category
+    include ROXML
+    xml_reader :domain, from: :attr
+    xml_reader :nicename, from: :attr
+    xml_reader :name, from: :content
 
-		def category?
-			domain == 'category'
-		end
-	end
+    def category?
+      domain == 'category'
+    end
 
-	class PostMeta
-		include ROXML
-		xml_reader :meta_key, from: 'wp:meta_key'
-		xml_reader :meta_value, from: 'wp:meta_value', cdata: true
-	end
+    def tag?
+      domain == 'post_tag'  
+    end
+  end
 
-	class Item
-		include ROXML
+  class PostMeta
+    include ROXML
+    xml_reader :meta_key, from: 'wp:meta_key'
+    xml_reader :meta_value, from: 'wp:meta_value', cdata: true
+  end
+
+  class Item
+    include ROXML
 
     xml_reader :title, from: 'title'
+    xml_reader :nicetitle, from: 'wp:post_name'
     xml_reader :pub_date, from: 'pubDate'
     xml_reader :status, from: 'wp:status'
     xml_reader :content, from: 'content:encoded', cdata: true
@@ -32,40 +37,39 @@ class Wortru
     xml_reader :categories, from: 'category', as: [Category]
     xml_reader :post_metas, from: 'wp:postmeta', as: [PostMeta]
 
-
     def published?
-    	status == 'publish'
+      status == 'publish'
     end
 
     def post?
-    	post_type == 'post'
+      post_type == 'post'
     end
 
     def true_categories
-    	categories.select(&:category?)
+      categories.select(&:category?)
     end
 
-    def meta_post_shop_reference
-    	post_meta = post_metas.find{|pm| pm.meta_key == 'post_shop_reference'}
-    	post_meta.meta_value if post_meta
+    def tags
+      categories.select(&:tag?)
+    end    
+
+    %w(post_shop_reference post_shop_type post_image).each do |name|
+      send :define_method, "meta_#{name}" do
+        post_meta = post_metas.find{|pm| pm.meta_key == name}
+        post_meta.meta_value if post_meta      
+      end
     end
 
-    def meta_post_shop_type
-    	post_meta = post_metas.find{|pm| pm.meta_key == 'post_shop_type7'}
-    	post_meta.meta_value if post_meta
-    end
+  end
 
-	end
-
-
-	def initialize(file_path)
-		raise 'No se ha introducido ninguna ruta de archivo' unless file_path
+  def initialize(file_path)
+    raise 'No se ha introducido ninguna ruta de archivo' unless file_path
 
     f = File.open(file_path)
     @reader = Nokogiri::XML::Reader(f)
   end
 
-	def each(&block)
+  def each(&block)
     @reader.each do |node|
       if @reader.node_type == 1 && @reader.name == 'item'
         str = @reader.outer_xml
@@ -73,6 +77,5 @@ class Wortru
       end
     end
   end
-
 
 end
